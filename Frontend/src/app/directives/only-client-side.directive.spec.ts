ac4b0ff6588c295c05562fdeda94d048d51650c9
Bart de Bever <bartdebever112@gmail.com>
2023-01-02 21:19:54 +0100

    Remove tests files from angular

--- Frontend/src/app/directives/only-client-side.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnlyClientSideDirective } from './only-client-side.directive';
-
-describe('OnlyClientSideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OnlyClientSideDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- Frontend/src/app/directives/video-autoplay-muted.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { VideoAutoplayMutedDirective } from './video-autoplay-muted.directive';
-
-describe('VideoAutoplayMutedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new VideoAutoplayMutedDirective();
-    expect(directive).toBeTruthy();
-  });
-});
