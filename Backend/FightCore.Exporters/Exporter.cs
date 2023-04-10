ed154dff551c1747a48fd4184f90cf2a63a82aa0
Bart de Bever <bartdebever112@gmail.com>
2023-04-10 22:39:23 +0200

    Open source all data in easy formats

---
+++ Backend/FightCore.Exporters/Exporter.cs
@@ -0,0 +1,42 @@
+﻿using CsvHelper.Configuration;
+using CsvHelper;
+using Newtonsoft.Json;
+using System.Collections;
+using System.Globalization;
+
+namespace FightCore.Exporters
+{
+	public static class Exporter
+	{
+		private static readonly JsonSerializerSettings _noNestedJsonSettings = new()
+		{
+			Formatting = Formatting.None,
+			ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+		};
+
+		public static async Task Export(IEnumerable toExport, string folder, string name)
+		{
+			if (!Directory.Exists(folder))
+			{
+				Directory.CreateDirectory(folder);
+			}
+
+			var movesJson = JsonConvert.SerializeObject(toExport, _noNestedJsonSettings);
+			var jsonPath = Path.Combine(folder, $"{name}.json");
+			Console.WriteLine($"Writing to {jsonPath}");
+			await File.WriteAllTextAsync(jsonPath, movesJson);
+
+			var csvPath = Path.Combine(folder, $"{name}.csv");
+			await using var writer = new StreamWriter(csvPath, false);
+			await using var csv = new CsvWriter(writer, new CsvConfiguration(CultureInfo.InvariantCulture)
+			{
+				UseNewObjectForNullReferenceMembers = false,
+				IgnoreReferences = true
+			});
+
+			Console.WriteLine($"Writing to {csvPath}");
+
+			await csv.WriteRecordsAsync(toExport);
+		}
+	}
+}
---
+++ Backend/FightCore.Exporters/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FightCore.FrameData;
+using FightCore.Repositories;
+using FightCore.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using FightCore.Exporters;
+
+Console.WriteLine("Hello, World!");
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+var dbContextOptions =
+	new DbContextOptionsBuilder<FrameDataContext>().UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+var dbContext = new FrameDataContext(dbContextOptions.Options);
+
+var repository = new CharacterRepository(dbContext);
+var services = new CharacterService(repository);
+
+var export = await services.ExportAll();
+
+var folder = configuration["Exports:Folder"];
+
+await Exporter.Export(export, folder, "characters");
+
+foreach (var character in export)
+{
+	var moves = character.Moves.Select(move =>
+	{
+		move.Character = null;
+		move.CharacterId = default;
+		return move;
+	});
+
+	await Exporter.Export(moves, Path.Combine(folder, character.NormalizedName), "moves");
+}
