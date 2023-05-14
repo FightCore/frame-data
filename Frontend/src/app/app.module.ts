bf9b6d801815a768724cea87ba7a260e656d8cd3
Bart de Bever <bartdebever112@gmail.com>
2023-05-14 21:11:59 +0200

    Hydration and last updates

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -82,7 +82,7 @@ import { FormsModule } from '@angular/forms';
     EffectsModule.forRoot([FrameDataEffects, UserSettingsEffects]),
     SharedModule,
   ],
-  providers: [],
+  providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
